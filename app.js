faae3fb53bf128e4ddd6a970bf70265d8b8314dc
joshyeom <jhyeom95@daum.net>
2022-11-18 16:21:29 +0900

    crawler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 const axios = require("axios");
+//crawler
 const cheerio = require("cheerio");
 
 app.set("view engine", "ejs");
