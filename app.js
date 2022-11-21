afbf826126da392808d8854b839369b8436c0f53
whqngml <archangel67@naver.com>
2022-11-21 09:47:17 +0900

    login api, search api 완료

--- app.js
+++ app.js
@@ -9,12 +9,11 @@ var client_secret = "FlW5KvYsgW";
 app.set("view engine", "ejs");
 app.use("/views", express.static(__dirname + "/views"));
 app.use("/static", express.static(__dirname + "/static"));
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const indexRouter = require("./routes");
+app.use("/", indexRouter);
 
 app.get("/", (req, res) => {
   res.render("index");
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-const express = require('express');
-const controller = require('../controller/Cvisitor');
+const express = require("express");
+const controller = require("../controller/Cvisitor");
 const router = express.Router();
 
 // 기본주소: localhost:PORT
 
 // GET / => localhost:PORT/
-router.get('/', controller.main);
+router.get("/", controller.main);
+router.get("/getCustomers", controller.getCustomers);
+router.get("/getOrderlists", controller.getOrderlists);
+router.get("/getTotal", controller.getTotal);
+router.get("/getSql", controller.getSql);
 
+router.delete("/delete/customer", controller.deleteCustomer);
+// get, post 등의 순서로 할지 페이지의 순서대로 할지 보기 좋게 정렬
+
+module.exports = router;
