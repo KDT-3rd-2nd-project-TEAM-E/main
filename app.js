9121437789cab8e1d69856cb3827e19c932a1d3a
wynsumhi <gusdk7244@naver.com>
2022-11-23 15:27:43 +0900

    app.js 오류수정

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.get("/", (req, res) => {
 // main
 app.get("/main", (req, res) => {
   res.render("main", {activeMenu : "main"});
+});
 
 app.get("/testlogin", (req, res) => {
   res.render("testlogin");
