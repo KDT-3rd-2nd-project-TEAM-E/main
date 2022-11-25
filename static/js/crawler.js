2a8064165900770c08f9a8f9e0d1f1570aefbdae
joshyeom <jhyeom95@daum.net>
2022-11-25 20:25:05 +0900

    gauge complete

--- static/js/crawler.js
+++ static/js/crawler.js
@@ -1,6 +1,87 @@
 // function crawler 크롤러 값 리스트에 작성
 const form = document.forms["search"];
 
+window.onload = (event) => {
+  event.preventDefault();
+  console.log(1);
+  axios({
+    method: "POST",
+    url: "/",
+    data: {
+      search: "스타벅스",
+    },
+  })
+    .then((res) => {
+      return (data = res.data);
+    })
+    .then((data) => {
+      const keys = Object.keys(data);
+      const value = [];
+      for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        value[i] = data[key];
+      }
+
+      const foodsInformation = document.querySelectorAll(".foods-information");
+
+      if (foodsInformation == null) {
+        for (let i = 0; i < value[0].length; i++) {
+          const html = `
+  <li class="foods-information">
+    <dl>
+    <dt class="food-name">${value[0][i].title}</dt>
+    <dd class="food-brand ">${value[0][i].brand}</dd>
+    <dd class="food-vol">${value[0][i].amount}</dd>
+    </dl>
+    <div class="cal-box clearfix">
+      <p class="food-kcal">${value[0][i].kcal}kcal</p>
+    <p class="food-car">탄수화물 ${value[0][i].carbs}g</p>
+    <p class="food-protein">단백질 ${value[0][i].protein}g</p>
+    <p class="food-fat">지방 ${value[0][i].fat}g</p>
+    </div>
+    <div class="button-box">
+      <div class="select" onclick="addKcal(${value[0][i].kcal},${value[0][i].carbs},${value[0][i].protein},${value[0][i].fat});"><i class="fas fa-plus"></i></div>
+      <div class="delete" onclick="minusKcal(${value[0][i].kcal},${value[0][i].carbs},${value[0][i].protein},${value[0][i].fat});"><i class="fas fa-minus"></i></div>
+    </div><hr>
+    </li>
+    `;
+
+          const foodList = document.querySelector(".food-list");
+          foodList.insertAdjacentHTML("afterbegin", html);
+        }
+      } else {
+        for (let i = 0; i < foodsInformation.length; i++) {
+          foodsInformation[i].parentNode.removeChild(foodsInformation[i]);
+        }
+        for (let i = 0; i < value[0].length; i++) {
+          const html = `
+  <li class="foods-information">
+    <dl>
+      <dt class="food-name">${value[0][i].title}</dt>
+      <dd class="food-brand ">${value[0][i].brand}</dd>
+      <dd class="food-vol">${value[0][i].amount}</dd>
+      </dl>
+  <div class="cal-box clearfix">
+    <p class="food-kcal">${value[0][i].kcal}kcal</p>
+    <p class="food-car">탄수화물 ${value[0][i].carbs}g</p>
+    <p class="food-protein">단백질 ${value[0][i].protein}g</p>
+    <p class="food-fat">지방 ${value[0][i].fat}g</p>
+    </div>
+  <div class="button-box">
+    <div class="select" onclick="addKcal(${value[0][i].kcal},${value[0][i].carbs},${value[0][i].protein},${value[0][i].fat});"><i class="fas fa-plus"></i></div>
+    <div class="delete" onclick="minusKcal(${value[0][i].kcal},${value[0][i].carbs},${value[0][i].protein},${value[0][i].fat});"><i class="fas fa-minus"></i></div>
+  </div>
+  <hr>
+    </li>
+    `;
+
+          const foodList = document.querySelector(".food-list");
+          foodList.insertAdjacentHTML("afterbegin", html);
+        }
+      }
+    });
+};
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   axios({
@@ -38,7 +119,10 @@ form.addEventListener("submit", (event) => {
     <p class="food-protein">단백질 ${value[0][i].protein}g</p>
     <p class="food-fat">지방 ${value[0][i].fat}g</p>
     </div>
-    <div class="select" onclick="addKcal(${value[0][i].kcal});"><i class="fas fa-check"></i></div>
+    <div class="button-box">
+      <div class="select" onclick="addKcal(${value[0][i].kcal},${value[0][i].carbs},${value[0][i].protein},${value[0][i].fat});"><i class="fas fa-plus"></i></div>
+      <div class="delete" onclick="minusKcal(${value[0][i].kcal},${value[0][i].carbs},${value[0][i].protein},${value[0][i].fat});"><i class="fas fa-minus"></i></div>
+    </div>
     <hr>
     </li>
     `;
@@ -64,7 +148,10 @@ form.addEventListener("submit", (event) => {
     <p class="food-protein">단백질 ${value[0][i].protein}g</p>
     <p class="food-fat">지방 ${value[0][i].fat}g</p>
     </div>
-    <div class="select" onclick="addKcal(${value[0][i].kcal},${value[0][i].carbs},${value[0][i].protein},${value[0][i].fat});"><i class="fas fa-check"></i></div>
+    <div class="button-box">
+      <div class="select" onclick="addKcal(${value[0][i].kcal},${value[0][i].carbs},${value[0][i].protein},${value[0][i].fat});"><i class="fas fa-plus"></i></div>
+      <div class="delete" onclick="minusKcal(${value[0][i].kcal},${value[0][i].carbs},${value[0][i].protein},${value[0][i].fat});"><i class="fas fa-minus"></i></div>
+    </div>
     <hr>
     </li>
     `;
